7c437a03bb70b05df139d683f3b3ea5ac04318e2
HansHuang <hys0550@sina.com>
2019-02-24 22:02:41 +0800

    basic buind in processing

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const http = require('http'),
     Koa = require('koa'),
-    koaBody = require('koa-body'),
-    liveConfig = require('liveconfig')
+    setupMiddleware = require('./lib/middleware'),
+    Config = require('./lib/config')
+
 
 process.on('uncaughtException', err => {
     console.error('global process error', err)
 })
-
-const Config = liveConfig(__dirname)['mock-server']
 
 process.env.NODE_ENV = (process.env.NODE_ENV || 'development').trim()
 const httpPort = Number(process.env.PORT || Config.port)
@@ -20,63 +19,13 @@ const httpPort = Number(process.env.PORT || Config.port)
 function startServer() {
 
     const app = new Koa()
-    setMiddleware(app)
+    setupMiddleware(app)
 
     const server = app.listen(httpPort)
     server.on('listening', onListening)
     server.on('error', onError)
 
     return server
-}
-
-/**
- * @param {Koa} app
- */
-function setMiddleware(app) {
-    app.use(catchGlobalError)
-        // .use(getKoaLogger('http'))
-        // .use(setViewRender(path.resolve(__dirname, 'views')))
-        .use(koaBody())
-        // .use(router)
-        .use(processNotFound)
-}
-
-
-/**
- * @param {Koa.Context} ctx
- * @param {Function} next
- */
-async function catchGlobalError(ctx, next) {
-    try {
-        await next()
-    } catch (error) {
-        ctx.status = error.status | 500
-        let msg = process.env.NODE_ENV === 'development' ? error.message : '500: Server Internal Error'
-        ctx.body = { error: msg }
-        ctx.app.emit('error', error, ctx)
-    }
-}
-
-/**
- * @param {Koa.Context} ctx
- */
-async function processNotFound(ctx) {
-    ctx.status = 404;
-
-    switch (ctx.accepts('html', 'json')) {
-        case 'html':
-            ctx.type = 'html'
-            await ctx.render('404', Config.App)
-            break;
-        case 'json':
-            ctx.body = {
-                error: '404'
-            }
-            break;
-        default:
-            ctx.type = 'text'
-            ctx.body = 'Page Not Found'
-    }
 }
 
 /**
