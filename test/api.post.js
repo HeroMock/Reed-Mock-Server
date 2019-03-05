014d78097e71d088cb11eb3aa04f24f6f680a1c7
HansHuang <hys0550@sina.com>
2019-03-05 08:18:15 +0800

    adding api post

--- test/api.post.js
+++ test/api.post.js
@@ -12,12 +12,28 @@ describe('HTTP server is working', () => {
         server = app.startServer()
     })
 
-    it('POST /api/ with empty body', () => {
+    it('POST with empty body', () => {
+        return request(server)
+            .post('/api/')
+            .send({})
+            .expect('Content-Type', /json/)
+            .expect(400)
+    })
 
+    it('POST with non-object body', () => {
+        return request(server)
+            .post('/api/whatever/')
+            .send('hello')
+            .expect('Content-Type', /json/)
+            .expect(400)
     })
 
     it('POST /api/ with object (no single key)', () => {
-
+        return request(server)
+            .post('/api/')
+            .send({ name: 'foo', value: 'bar' })
+            .expect('Content-Type', /json/)
+            .expect(400)
     })
 
     it('POST /api/ with object (single key)', () => {
@@ -25,6 +41,18 @@ describe('HTTP server is working', () => {
     })
 
     it('POST /api/:name with object (no single key)', () => {
+
+    })
+
+    it('POST /api/ with array', () => {
+        return request(server)
+            .post('/api/')
+            .send([1, 2, 3])
+            .expect('Content-Type', /json/)
+            .expect(400)
+    })
+
+    it('POST /api/:name with array', () => {
 
     })
 
