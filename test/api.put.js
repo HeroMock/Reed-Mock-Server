0011f0bdecefe20c086359b6c6fa43691c80794e
Hans Huang <hys0550@sina.com>
2019-04-04 07:29:54 +0800

    add test cases

--- test/api.put.js
+++ test/api.put.js
@@ -38,27 +38,85 @@ describe('Restful API PUT', () => {
     })
 
     it('PUT /api/:entity/ (plural resource) without id in body', () => {
-
+        return request(server)
+            .put('/api/feedbacks')
+            .send({ comment: 'great', userId: 55, timestamp: '20190707 09:23:23' })
+            .expect('Content-Type', /json/)
+            .expect(404)
     })
 
     it('PUT /api/:entity/ (single resource)', () => {
+        const res = await request(server)
+            .put('/api/profile')
+            .send({ defaultTimezone: 'UTC+8' })
+            .expect('Content-Type', /json/)
+            .expect(200)
+        assert.strictEqual('undefined', typeof res.body.enableComment)
+        assert.strictEqual('UTC+8', res.body.defaultTimezone)
 
+        const res2 = await request(server)
+            .get('/api/profile')
+            .expect(200)
+        assert.strictEqual('UTC+8', res2.body.defaultTimezone);
     })
 
     it('PUT /api/:entity/ (plural resource) with id in body', () => {
+        const res0 = await request(server).get('/api/users/5')
+        assert.notStrictEqual('Hans Huang', res0.body.name)
+        assert.strictEqual('string', typeof res0.body.email)
 
+        const res = await request(server)
+            .put('/api/users')
+            .send({ id: 5, name: 'Hans Huang', company: 'xxxxx' })
+            .expect('Content-Type', /json/)
+            .expect(200)
+        assert.strictEqual('Hans Huang', res.body.name)
+        assert.strictEqual('xxxxx', res.body.company)
+        assert.strictEqual('undefined', typeof res.body.email)
+
+        const res2 = await request(server)
+            .get('/api/users/5')
+            .expect(200)
+        assert.strictEqual('Hans Huang', res2.body.name)
     })
 
     it('PUT /api/:entity/ (plural resource) with array body', () => {
+        const res = await request(server)
+            .put('/api/users')
+            .send([{ id: 10, name: 'Hans' }, { id: 20, name: 'Huang' }])
+            .expect('Content-Type', /json/)
+            .expect(200)
+        assert.strictEqual(2, res.body.length)
 
+        const res2 = await request(server)
+            .get('/api/users/10')
+            .expect(200)
+        assert.notStrictEqual('Hans', res2.body.name)
     })
 
     it('PUT /api/:entity/:id (plural resource)', () => {
+        const res = await request(server)
+            .put('/api/users/30')
+            .send({ name: 'Hans Huang', job: 'coder' })
+            .expect('Content-Type', /json/)
+            .expect(200)
+        assert.strictEqual('Hans Huang', res.body.name)
+        assert.strictEqual('coder', res.body.job)
 
+        const res2 = await request(server)
+            .get('/api/users/5')
+            .expect(200)
+        assert.strictEqual('Hans Huang', res2.body.name)
+        assert.strictEqual('coder', res2.body.job)
+        assert.strictEqual('undefined', typeof res.body.email)
     })
 
     it('PUT /api/:entity/:id (single resource)', () => {
-
+        return request(server)
+            .put('/api/profile/5')
+            .send({ defaultTimezone: 'UTC+8' })
+            .expect('Content-Type', /json/)
+            .expect(404)
     })
 
     after(async () => {
