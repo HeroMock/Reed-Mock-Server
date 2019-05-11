1614bffe1b9cc342fcb3da7a3314d2692773c936
HansHuang <hys0550@sina.com>
2019-05-11 23:47:48 +0800

    add cli commands

--- cli/index.js
+++ cli/index.js
@@ -1,12 +1,38 @@
 #!/usr/bin/env node
 
-const program = require('commander')
+const path = require('path'),
+    fs = require('fs'),
+    { promisify } = require('util'),
+    program = require('commander')
 
 program
-    .version(require('../package.json').version, '-v, --version')
-    .command('init', 'init config for mock server').action(initServer)
+    .version(require('../package.json').version)
+
+program
+    .command('init')
+    .description('init config for mock server')
+    .action(initServer)
+
+program
+    .command('start [config]')
+    .description('start mock server')
+    .action(startServer)
+
+program
     .parse(process.argv)
 
+
 function initServer() {
-    console.log('init server')
+    ['mock-server.json', 'json.hbs'].forEach(s => {
+        fs.copyFile(
+            path.join(__dirname, '..', s),
+            path.join(process.cwd(), s),
+            e => e ? console.error(e.message) : console.log(`mock server ${s} initialized`)
+        )
+    })
+}
+
+function startServer(config) {
+    process.env.MockConfig = config || ''
+    require('../index').startServer()
 }
