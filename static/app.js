a502a3bc12becbe197933113c3d368dc74c4de68
HansHuang <hys0550@sina.com>
2019-05-29 08:22:25 +0800

    add test cases

--- static/app.js
+++ static/app.js
@@ -1,7 +1,4 @@
 setInterval(() => {
-    let r = Math.floor(256 * Math.random()),
-        g = Math.floor(256 * Math.random()),
-        b = Math.floor(256 * Math.random())
-
-    document.body.style.backgroundColor = `rgb(${r},${g},${b})`
+    let p = document.getElementsByTagName('p')[0]
+    p.innerText = Date()
 }, 1000);
---
+++ test/static.js
@@ -0,0 +1,44 @@
+const request = require('supertest'),
+    assert = require('assert')
+
+process.env.NODE_ENV = 'test'
+
+const app = require('../index')
+
+describe('Static HTTP Server', () => {
+    let server;
+
+    beforeEach(() => {
+        server = app.startServer()
+    })
+
+    it('1. Request index page', async () => {
+        const res = await request(server)
+            .get('/')
+            .expect('Content-Type', /html/)
+            .expect(200)
+
+        assert.strictEqual(Number(res.header['content-length']) > 0, true)
+    })
+
+    it('2. Request non-html file', async () => {
+        const res = await request(server)
+            .get('/app.js')
+            .expect('Content-Type', /javascript/)
+            .expect(200)
+
+        assert.strictEqual(Number(res.header['content-length']) > 0, true)
+    })
+
+    it('2. Request non-existed file', async () => {
+        const res = await request(server)
+            .get('/no-existed.js')
+            .expect(404)
+
+        assert.strictEqual(Number(res.header['content-length']) > 0, true)
+    })
+
+    afterEach(async () => {
+        await server.close()
+    })
+})
