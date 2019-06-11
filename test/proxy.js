87f4e87c3a8fdfacf9b8cf2b361d66ca34f3cb93
HansHuang <hys0550@sina.com>
2019-06-11 21:43:41 +0800

    implementating web proxy

--- test/proxy.js
+++ test/proxy.js
@@ -1,19 +1,35 @@
-const request = require('supertest')
+const http = require('http'),
+    request = require('supertest')
 
 process.env.NODE_ENV = 'test'
 
 const app = require('../index')
 
 describe('HTTP Transparent Proxy', () => {
-    let server
+    let server, remoteServer, remoteReq
 
     before(() => {
         server = app.startServer()
     })
 
-    it('index page responds 200 ', done => {
+    beforeEach('start another server', () => {
+        remoteReq = new Promise(resolve => {
+            remoteServer = http.createServer((req, res) => {
+                res.writeHead(200, { 'Content-Type': 'text/plain' })
+                res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2))
+                res.end()
+                resolve(req)
+            }).listen(4000)
+        })
+    })
+
+    afterEach('stop remove server', () => {
+        remoteServer.close()
+    })
+
+    it('Proxy Get', done => {
         request(server)
-            .get('/proxy-foo')
+            .get('/proxy-foo/')
             .expect(200, done)
     })
 
